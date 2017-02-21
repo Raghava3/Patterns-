d24e3fd7f8cb5dd629cb5f7881fc028f8e8be3f4
raghav <1raghuram3@gmail.com>
2017-02-21 19:07:22 +0530

    This is E pattern program

---
+++ EPattern.java
@@ -0,0 +1,25 @@
+package com.pattern;
+
+public class EPattern {
+
+	public  static void main(String[] args)
+	{
+	for (int i = 0; i <5; i++)
+	{
+	
+		for (int j = 0; j <=5; j++) 
+		{
+if(i==0||i==2||i==4||j==0)
+			{
+            System.out.print("* ");	
+			}
+			else
+			System.out.print("  ");
+			
+		}System.out.println(""); 
+	
+	}
+	
+}
+	
+}
