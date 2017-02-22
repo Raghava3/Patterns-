f365386e3995773746dfb9af891b4e2d2afc4dc9
raghav <1raghuram3@gmail.com>
2017-02-22 09:41:37 +0530

    L pattern program

---
+++ LPattern.java
@@ -0,0 +1,26 @@
+package com.pattern;
+
+public class LPattern {
+	public  static void main(String[] args)
+	{
+	for (int i = 0; i <5; i++)
+	{
+	
+		for (int j = 0; j <=5; j++) 
+		{
+if(j==0||(i==4&&j<5))
+			{
+            System.out.print("* ");	
+			}
+			else
+			{
+			System.out.print("  ");
+			}
+		}System.out.println(""); 
+	
+	}
+	
+}
+	
+
+}
