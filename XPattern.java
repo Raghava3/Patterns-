3a920d343fdb37a95e9000ef61e2f4c5b01e23ce
raghav <1raghuram3@gmail.com>
2017-02-22 12:55:06 +0530

    this is x pattern

--- XPattern.java
+++ XPattern.java
@@ -2,4 +2,26 @@ package com.pattern;
 
 public class XPattern {
 
+	public static void main(String[] args)
+	{
+		{
+			for (int i = 0; i <5; i++)
+			{
+			
+				for (int j = 0; j <=5; j++) 
+				{
+if(i==j||((i+j)==4))					{
+		            System.out.print("* ");	
+					}
+					else
+					{
+					System.out.print("  ");
+					}
+				}System.out.println(""); 
+			
+			}
+			
+		}
+	}
 }
+
