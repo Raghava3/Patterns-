415d3f5da4638d10e4b8cd2bdffa9261ed17af8e
raghav <1raghuram3@gmail.com>
2017-02-22 08:59:29 +0530

    this is  JPattern

---
+++ JPattern.java
@@ -0,0 +1,25 @@
+package com.pattern;
+
+public class JPattern {
+	public  static void main(String[] args)
+	{
+	for (int i = 0; i <5; i++)
+	{
+	
+		for (int j = 0; j <=5; j++) 
+		{
+      if(i==0||(i==4&&j<2)||j==2||(j==0&&i>2))
+			{
+            System.out.print("* ");	
+			}
+			else
+			{
+			System.out.print("  ");
+			}
+		}System.out.println(""); 
+	
+	}
+	
+}
+
+}
