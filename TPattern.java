1727960d52b0686c0e768801c6b4ad0d6bd29a36
raghav <1raghuram3@gmail.com>
2017-02-22 12:13:44 +0530

    this is t pattern prg

---
+++ TPattern.java
@@ -0,0 +1,25 @@
+package com.pattern;
+
+public class TPattern {
+	public  static void main(String[] args)
+	{
+	for (int i = 0; i <5; i++)
+	{
+	
+		for (int j = 0; j <=5; j++) 
+		{
+if(i==0||j==2)
+			{
+            System.out.print("* ");	
+			}
+			else
+			{
+			System.out.print("  ");
+			}
+		}System.out.println(""); 
+	
+	}
+	
+}
+
+}
